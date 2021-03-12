df25bcf7bec2244b16ac079a40c87d09e9d7368d
Artur Sudnik-Hrynkiewicz <j.sudnik-hrynkiewicz@shell.com>
2021-03-12 18:10:57 +0100

    popup, post and callback working example

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 
 require('dotenv-defaults').config();
 
-const axios                   = require('axios'),
-      chalk                   = require('chalk'),
-      express                 = require('express'),
-      morgan                  = require('morgan');
+const axios      = require('axios'),
+      bodyParser = require('body-parser'),
+      chalk      = require('chalk'),
+      express    = require('express'),
+      morgan     = require('morgan');
 
 const app  = express();
 const port = process.env.PORT || 3000;
@@ -24,6 +25,19 @@ app.use((req, res, next) => {
  * serving static files from ./public folder
  */
 app.use(express.static('public'))
+
+app.post('/post', bodyParser.urlencoded({extended: true}), (req, res) => {
+
+    console.log(`post data: ${JSON.stringify(req.body)}`);
+
+    const callbackData = {
+        timestamp: new Date().toISOString()
+    }
+
+    const targetUrl = '/callback.html' + '#' + new URLSearchParams(callbackData).toString();
+
+    setTimeout(() => res.redirect(targetUrl), 1000);
+});
 
 app.listen(port, async () => {
     debugLogger(`server is listening at http://localhost:${port}`);
